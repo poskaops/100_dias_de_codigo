23b8b9cb245282ee4af8a33899ad4d3c3df6f26e
joaoposka <115417441+poskaops@users.noreply.github.com>
2024-09-05 21:54:22 -0300

    Dia 2 - Tudo em ordem

---
+++ dia002/dia002.py
@@ -0,0 +1,8 @@
+# Desafio: Escreva um Programa que recebe dois números do usuário e imprima a soma deles.
+
+n1 = int(input("Primeiro Número: "))
+n2 = int(input("Segundo Número: " ))
+
+resultado = n1 + n2
+
+print ("O resultado é  :", resultado)
